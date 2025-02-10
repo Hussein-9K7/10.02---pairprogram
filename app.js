0e0f9cca49c3dbbb8c663f10a937235e861f4af7
Hussein <alprstigehussein@gmail.com>
2025-02-10 16:50:42 +0200

    some changes

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-require('dotenv').config();  // تحميل البيئة من ملف .env
+require('dotenv').config();  
 const connectDB = require('./config/db');
 const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 
-// استيراد middleware
+
 const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
 const morgan = require("morgan");
 
-// استخدام morgan لتسجيل تفاصيل الطلبات في الـ console
+
 app.use(morgan("dev"));
 
-// الاتصال بقاعدة البيانات
+
 connectDB();
 
-// استخدام express.json لتحليل جسم الطلبات التي تحتوي على JSON
+
 app.use(express.json());
 
-// تعريف المسارات
+
 app.use("/api/tours", tourRouter);
 app.use("/api/users", userRouter); 
 
-// مسار لمحاكاة حدوث خطأ
+
 app.get('/error', (req, res, next) => {
   // إنشاء خطأ
   const error = new Error("Something went wrong!");
-  // تمرير الخطأ إلى middleware الخاص بالتعامل مع الأخطاء
+
   next(error);
 });
 
-// معالجة المسارات غير المعروفة (404)
+
 app.use(unknownEndpoint);
 
-// استخدام errorHandler بعد كل شيء للتعامل مع الأخطاء
+
 app.use(errorHandler);
 
-// تحديد المنفذ (port) من البيئة أو استخدام 4000 كبديل
+
 const port = process.env.PORT || 4000;
 
-// بدء تشغيل الخادم
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/api/tours , http://localhost:${port}/api/users`);
 });
--- middleware/customMiddleware.js
+++ middleware/customMiddleware.js
@@ -1,26 +1,23 @@
-// middleware/customMiddleware.js
-
-// Middleware لتسجيل تفاصيل الطلبات
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
   console.log("Body:  ", request.body);
   console.log("---");
-  next(); // الانتقال إلى الميدلوير التالي
+  next(); 
 };
 
-// Middleware للتعامل مع المسارات غير المعروفة (404)
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-// Middleware للتعامل مع الأخطاء
+
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message); // طباعة الخطأ في السيرفر
+  console.error(error.message); 
   
-  // إرسال استجابة للمستخدم مع حالة 500 ورسالة عامة
+
   response.status(500).json({
-    message: "Network problem", // يمكن تغيير هذه الرسالة إذا أردت إظهار تفاصيل الخطأ للمستخدم
+    message: "Network problem", 
   });
 };
 
